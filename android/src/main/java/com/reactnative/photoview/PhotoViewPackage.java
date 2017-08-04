e5da63392cb4e1e7af791791b5076379fedb02ad
Pham Thanh Tu <tubackkhoa@gmail.com>
2017-08-05 04:41:34 +0700

    Update PhotoViewPackage.java
    RN 0.47 deprecated

--- android/src/main/java/com/reactnative/photoview/PhotoViewPackage.java
+++ android/src/main/java/com/reactnative/photoview/PhotoViewPackage.java
@@ -15,7 +15,7 @@ import java.util.List;
  * @version 1.0
  */
 public class PhotoViewPackage implements ReactPackage {
-    @Override
+    // RN 0.47 deprecated
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
